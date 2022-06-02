5b23269dc3dedc7df69b17eb2616c0cd9ddf1456
golemiam <52841560+golemiam@users.noreply.github.com>
2022-06-02 16:59:19 -0600

    Update index.asm
    made some additional changes, still trying to get it to work

--- index.asm
+++ index.asm
@@ -7,17 +7,19 @@ num1
     ADD R1, R1, R5 ; convert the ASCII to an integer
     IN
     ADD R0, R0, R5 ; Convert the ASCII to an integer
-    JSR twos_comp    
+    JSR TWOSCOMP  
     ADD R2, R0, #0 ; Move to R2
 
 num2 
+    ADD R7, R7, #0
     ADD R2, R0, R2 ; Convert R2 into 2s compliment
     JSR Eval
     HALT
-    
+    BRnzp Done
 Eval
-    ADD R3, R2, R1 ;
-    ADD R3, R3, #0
+    ADD R3, R2, R1 ; Subtract
+    ADD R3, R3, #0 ; Set to evaluate
+    ADD R4, R4, #1 ; Count up
     BRp Eval
     OUT
     RET
@@ -28,7 +30,9 @@ Eval
 ASCII   .FILL x30           ; mask to add to convert to ASCII
 NEGASCII .FILL xFFD0         ; negated ASCII maask (-x30)
 
-twos_comp ; Use R0 as the parameter
-    .fill x3100
-    
+TWOSCOMP
+    NOT R0, R0
+    ADD R0, R0, #1
+    RET
+Done
     .END
